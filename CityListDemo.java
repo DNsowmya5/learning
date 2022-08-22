629c13f6a6c7836873561c515fbd3201520b81bf
Syed Aisha <111122737+DNsowmya5@users.noreply.github.com>
2022-08-22 11:24:03 +0530

    Add files via upload

---
+++ CityListDemo.java
@@ -0,0 +1,53 @@
+package collections;
+import java.util.*;  
+
+public class CityListDemo {
+
+	 public static void main(String args[]){  
+	  ArrayList<String> cityList=new ArrayList<String>();   
+	      cityList.add("GUNTUR");    
+	      cityList.add("VIJAYAWADA");    
+	      cityList.add("ONGOLE");    
+	      cityList.add("NELLUR"); 
+	      cityList.add("VISHAKAPATNAM");    
+	      cityList.add("KARNOOL");    
+	      cityList.add("NARASARAOPET");    
+
+	        
+	      System.out.println(cityList);  
+	      cityList.add(2,"CHILAKALURIPET");
+	      System.out.println(cityList);  
+			cityList.set(0, "HYDERABAD");
+			cityList.add(1, "VIJAYAWADA");
+			cityList.set(2,"GUNTUR");
+		      System.out.println(cityList);  
+				System.out.println(cityList.contains("nandhayla"));
+				cityList.remove("GUNTUR");
+			      System.out.println(cityList);  
+
+				System.out.println(cityList.size());
+				System.out.println(cityList.isEmpty());
+				ArrayList<String> districtName= new ArrayList<String>();
+				districtName.add("HYD");
+				districtName.add("NTR");
+				districtName.add("PALNADU");
+				districtName.add("PRASAM");
+				districtName.add("SPSR NELLOR");
+				districtName.add("VISHAKAPATNAM");
+				districtName.add("KARNOOL");
+				districtName.add("PALNADU");
+			      System.out.println(districtName);  
+				cityList.addAll(1, districtName);
+			      System.out.println(cityList);  
+
+				
+
+				
+
+
+
+
+
+
+	 }  
+	} 
